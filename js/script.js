6d56bdbf647032ea935e8a7534574ad3e5290972
Sergey <razuvaj@gmail.com>
2020-05-26 15:33:40 +0300

    add cycles and conditions

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     };
 
 
-const a = prompt('Какой последний фильм вы смотрели?', ''),
-    b = +prompt('На сколько оцените его?', ''),
-    c = prompt('Какой последний фильм вы смотрели?', ''),
-    d = +prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой последний фильм вы смотрели?', ''),
+          b = +prompt('На сколько оцените его?', '');
+    
+    if (!a || !b || b.length > 50) {
+        console.log ('что-то пошло не так');
+        i--;
+    } else {
+        personalMovieDB.movies[a] = b;
+        console.log ('ok');
+    }; 
+}
 
 console.log (personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log ("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log ("Батенька, да вы киноман!!");
+} else {
+    console.log ("Не, ну так не бывает");
+};
+
