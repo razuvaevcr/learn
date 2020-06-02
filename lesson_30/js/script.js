eafcf91fd4f0f69fb15c502cfda2016eaab2c471
Sergey <razuvaj@gmail.com>
2020-06-02 17:52:00 +0300

    less30 use events on page

--- lesson_30/js/script.js
+++ lesson_30/js/script.js
@@ -1,71 +1,96 @@
-/* Задания на урок:
+"use strict";
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+document.addEventListener('DOMContentLoaded', () => {
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
 
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkBox = addForm.querySelector('[type="checkbox"]');
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
 
-//1
-adv.forEach(item => {
-    item.remove();
+    addForm.addEventListener('submit', (event) => { // обработчик события отправки формы
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorit = checkBox.checked;
+
+        if(newFilm) {
+
+            if (newFilm.length > 21){
+                newFilm = `${newFilm.substring(0, 22)}...`; //обрезаем название до 21-го символа
+            }
+
+            if (favorit) {
+                console.log('Добавляем любимый фильм')
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            createMovielist(movieDB.movies, movieList);
+        };
+
+        event.target.reset(); // очищает форму (цель события)
+
+    });
+
+
+    const deleteAdv = (arr) => { //функция удаления рекламы
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        genre.textContent = "драма";
+
+        poster.style.background = "url('/img/bg.jpg')";
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovielist(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i+1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovielist(movieDB.movies, movieList);
+            });
+        });
+    };
+
+
+    deleteAdv(adv);
+    makeChanges();
+    
+    createMovielist(movieDB.movies, movieList);
+
 });
-
-//2
-genre.textContent = "драма";
-
-//3
-poster.style.background = "url('/img/bg.jpg')";
-
-//4 5
-movieList.innerHTML = '';
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i+1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
-
-const inpt = document.querySelector('.adding__input').value,
-    btn = document.querySelector('button');
-
-
-
-btn.addEventListener('click', () => {
-    movieDB.movies.push(inpt);
-});
-
-
-console.log(movieDB.movies)
-console.log(btn)
-
